fcc68b12187e2dfb25cb463c585ae76615702985
ypcouc <714782688@qq.com>
2018-09-12 16:54:25 +0800

    java常用工具初次提交

---
+++ src/main/java/com/ypc/CommonToolApplication.java
@@ -0,0 +1,12 @@
+package com.ypc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonToolApplication.class, args);
+    }
+}
